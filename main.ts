feaa8b06af50db87f09a20e9d778ecfc20f4a5bd
filesfor <143867560+filesfor@users.noreply.github.com>
2024-10-19 08:48:27 +0800

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -2,8 +2,15 @@ scene.onHitWall(SpriteKind.Player, function (sprite, location) {
     mySprite.vy = 0
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    mySprite.vy = -50
+    if (canJump == 1) {
+        mySprite.vy = -160
+        canJump = 0
+        mySprite.vy = 0
+        pause(3000)
+        canJump = 1
+    }
 })
+let canJump = 0
 let mySprite: Sprite = null
 mySprite = sprites.create(img`
     . . . 1 1 1 1 1 1 . . . . . . . 
@@ -23,7 +30,7 @@ mySprite = sprites.create(img`
     . 2 2 2 2 . . . 2 2 2 2 . . . . 
     2 2 2 2 2 . . . 2 2 2 2 2 . . . 
     `, SpriteKind.Player)
-tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 17))
+tiles.placeOnTile(mySprite, tiles.getTileLocation(2, 13))
 controller.moveSprite(mySprite, 100, 0)
 mySprite.vy = 100
 tiles.setCurrentTilemap(tilemap`level`)
@@ -31,6 +38,7 @@ tiles.setTileAt(tiles.getTileLocation(0, 14), assets.tile`myTile`)
 tiles.setTileAt(tiles.getTileLocation(0, 15), assets.tile`myTile`)
 tiles.setTileAt(tiles.getTileLocation(1, 14), assets.tile`myTile`)
 scene.cameraFollowSprite(mySprite)
+canJump = 1
 forever(function () {
     mySprite.y += 2
 })
